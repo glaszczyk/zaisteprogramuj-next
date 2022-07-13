bef4c2010255c1482d2a35c6f4dc3e10a9451152
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-07-13 22:50:59 +0200

    add users dir with users and index (#2)
    * add users dir with users and index
    
    * Update workspace.xml

---
+++ pages/users/index.tsx
@@ -0,0 +1,5 @@
+const Users = () => {
+    return <div>To ja, Twój Users index.</div>
+};
+
+export default Users;
---
+++ pages/users/users.tsx
@@ -0,0 +1,5 @@
+const Users = () => {
+    return <div>To ja Twój plik Users!</div>
+}
+
+export default Users;
