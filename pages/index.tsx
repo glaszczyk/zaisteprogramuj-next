93a7ea043f3c93a84986a6b3157ee2e57d90cefe
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-07-19 13:31:22 +0200

    Add image and description. Add header navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,15 @@ import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import Head from "next/head";
 
+const DATA = {
+  description: `Maecenas dapibus egestas lacus a ornare. Proin iaculis, lacus id pellentesque rutrum, lectus orci ultrices purus, eu rutrum turpis dui nec nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer molestie ipsum mauris, nec
+    varius est cursus id. Integer ac velit sed elit euismod faucibus non sed arcu. Duis venenatis sollicitudin eros ut lacinia. In hac habitasse platea dictumst. Maecenas eget rhoncus nulla, a porta nibh. Fusce sed rutrum risus. Fusce erat arcu, pretium et sem
+    ultricies, ultrices euismod lacus. Duis tempus magna in elit facilisis, ut convallis felis tincidunt.`,
+  thumbnailUrl: "https://picsum.photos/id/256/600/400",
+  thumbnailAlt: "",
+  rating: 4.5,
+};
+
 const Home = () => {
   return (
     <div>
@@ -11,19 +20,9 @@ const Home = () => {
       <div className="flex flex-col min-h-screen max-w-4xl mx-auto">
         <Header />
         <main className="flex-grow p-6 grid md:grid-cols-2 gap-6">
-          <img src="https://picsum.photos/id/256/600/400" alt="" />
-          <p>
-            Maecenas dapibus egestas lacus a ornare. Proin iaculis, lacus id
-            pellentesque rutrum, lectus orci ultrices purus, eu rutrum turpis
-            dui nec nibh. Orci varius natoque penatibus et magnis dis parturient
-            montes, nascetur ridiculus mus. Integer molestie ipsum mauris, nec
-            varius est cursus id. Integer ac velit sed elit euismod faucibus non
-            sed arcu. Duis venenatis sollicitudin eros ut lacinia. In hac
-            habitasse platea dictumst. Maecenas eget rhoncus nulla, a porta
-            nibh. Fusce sed rutrum risus. Fusce erat arcu, pretium et sem
-            ultricies, ultrices euismod lacus. Duis tempus magna in elit
-            facilisis, ut convallis felis tincidunt.
-          </p>
+          <img src={DATA.thumbnailUrl} alt={DATA.thumbnailAlt} />
+          <p>{DATA.description}</p>
+          <p className="text-blue-600 font-bold">{DATA.rating}</p>
         </main>
         <Footer />
       </div>
