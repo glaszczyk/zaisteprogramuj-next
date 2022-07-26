0e905822ce1e6e5b0962836ed7930ddfc28b45cd
Grzegorz Laszczyk <info@grzegorzlaszczyk.pl>
2022-07-26 13:44:44 +0200

    Display products via CSR

---
+++ pages/products-csr.tsx
@@ -0,0 +1,50 @@
+import { Product } from "../components/Product";
+import { useQuery } from "react-query";
+
+export const getProducts = async () => {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data: StoreApiResponse[] = await response.json();
+  return data;
+};
+
+const ProductsCSRPage = () => {
+  const result = useQuery("products", getProducts);
+  if (result.isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (result.error || !result.data) {
+    return <div>Coś poszło nie tak...</div>;
+  }
+  return (
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {result.data.map((product) => (
+        <li key={product.id}>
+          <Product
+            data={{
+              title: product.title,
+              description: product.description,
+              thumbnailAlt: product.title,
+              thumbnailUrl: product.image,
+              rating: product.rating.rate,
+            }}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ProductsCSRPage;
+
+interface StoreApiResponse {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
